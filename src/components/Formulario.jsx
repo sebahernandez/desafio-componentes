0e0920d994ebc3b462cf751c898986640ed5fe40
MItPlaza <m.plazaweb@gmail.com>
2024-03-10 14:58:17 -0300

    Se hicieron validaciones en el nombre para que solo acepte letras y acentos, el email debe ser valido y la password debe contener al menos 1 mayúscula y 8 caracteres

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -16,16 +16,41 @@ export const Formulario = () => {
       (value) => value === ""
     );
     if (camposVacios) {
-      setMensaje("No pueden haber campos vacios.");
+      setMensaje("No pueden haber campos vacíos.");
       setVariant("danger");
+    } else if (!nombreValid(inputValues.nombre)){
+      setMensaje("El nombre debe contener solo letras.");
+      setVariant("warning");
+
+    } else if(!passValid(inputValues.password)){
+      setMensaje("La contraseña debe comenzar con mayúscula y cintener al menos 8 caracteres.");
+      setVariant("warning");
+    
     } else if (inputValues.password !== inputValues.confirmPassword) {
       setMensaje("Las contraseñas deben ser iguales.");
+      setVariant("warning");
+
+    } else if (!emailValid(inputValues.email)) {
+      setMensaje("El email no es válido.");
       setVariant("warning");
     } else {
       setMensaje("Usuario registrado correctamente.");
       setVariant("success");
     }
   };
+
+  const emailValid = (email) => {
+    const rgExp = /^[a-zA-Z0-9._]+@[a-z]+\.[a-z]{2,6}$/;
+    return rgExp.test(email);
+  };
+  const nombreValid = (nombre) => {
+    const rgExp2 =/^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ]+$/;
+    return rgExp2.test(nombre);
+  };
+  const passValid = (password) => {
+    const regex = /^(?=.*[A-Z]).{8,}$/;
+    return regex.test(password);
+  }
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
